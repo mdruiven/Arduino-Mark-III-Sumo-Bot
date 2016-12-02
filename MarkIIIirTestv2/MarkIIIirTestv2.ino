d7c8ca1d6c71e8caab76b636e7a7b3fc92efc54b
M Druiven <mdruiven@users.noreply.github.com>
2016-12-01 21:14:00 -0500

    Update MarkIIIirTestv2.ino

--- MarkIIIirTestv2/MarkIIIirTestv2.ino
+++ MarkIIIirTestv2/MarkIIIirTestv2.ino
@@ -1,8 +1,8 @@
-
+// Mike Druiven Nov 30, 2016
 // code to run Arduino Mark III robot with Sharp IR sensors
 #include <SharpIR.h>
-SharpIR sensorL(GP2Y0A21YK0F, A3);
-SharpIR sensorR(GP2Y0A21YK0F, A4);
+SharpIR sensorL(GP2Y0A21YK0F, A3); // Left sensor connected to A3
+SharpIR sensorR(GP2Y0A21YK0F, A4); // Right sesnor connected to A4
 void setup() {
 Serial.begin(9600);
 }
@@ -13,5 +13,5 @@ void loop() {
   Serial.print(disL);
   Serial.print("   ");
   Serial.println(disR);
-
+  // Print the left and right sensor values onto the serial monitor
 }
