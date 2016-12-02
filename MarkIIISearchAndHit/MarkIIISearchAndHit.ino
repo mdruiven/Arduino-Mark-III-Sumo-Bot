fd143c735fe8f7bb06adf00d304b4db511bbfab7
M Druiven <mdruiven@users.noreply.github.com>
2016-12-01 21:02:09 -0500

    Update MarkIIISearchAndHit.ino

--- MarkIIISearchAndHit/MarkIIISearchAndHit.ino
+++ MarkIIISearchAndHit/MarkIIISearchAndHit.ino
@@ -1,5 +1,7 @@
 /* 
-Drive and avoid walls
+Robot spins until an object about the same size as itself is placed nearby.
+The Robot will then move towards the object.
+Mike Druiven Nov. 30, 2016
 */ 
 
 #include <Servo.h> 
@@ -24,49 +26,49 @@ void setup()
 void loop() 
 {                              
    int disL=sensorL.getDistance();
-   int disR=sensorR.getDistance();
-   if (disL<20 && disR<20){
+   int disR=sensorR.getDistance(); //Get the left and right ip sensor values.
+   if (disL<20 && disR<20){ //Look for an object within range 20.
+    allStop(50); // If an object is detected stop spinning.
+    turnLeft(100); // Since it was spinning to the right it needs to turn back towards the object a bit.
     allStop(50);
-    turnLeft(100);
-    allStop(50);
-    forward(1000);
+    forward(1000); // Now run at the object.
    }
    else
    {
-    turnRight(100);
+    turnRight(100); // Otherwise keep spinning to the right.
    }
         
 } 
 
-void forward(int d)
+void forward(int d) // Drive forward for d milliseconds.
 {
     servoL.write(zeroL+s);
     servoR.write(zeroR-s);
     delay(d);          
 } 
 
-void reverse(int d)
+void reverse(int d)  //  Drive reverse for d milliseconds
 {
     servoL.write(zeroL-s);
     servoR.write(zeroR+s);
     delay(d);          
 } 
 
-void turnLeft(int d)
+void turnLeft(int d) // Spin to the left for d milliseconds.
 {
     servoL.write(zeroL-s);
     servoR.write(zeroR-s);
     delay(d);          
 } 
 
-void turnRight(int d)
+void turnRight(int d) // Spin to the right for d milliseconds.
 {
     servoL.write(zeroL+s);
     servoR.write(zeroR+s);
     delay(d);          
 } 
 
-void allStop(int d){
+void allStop(int d){ // Stop moving for d milliseconds.
   servoL.write(zeroL);
     servoR.write(zeroR);
     delay(d); 
