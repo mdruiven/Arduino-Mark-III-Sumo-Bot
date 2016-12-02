07e31fd01e27fcc2b0ce0ec52f4e50d1d86ff5fb
M Druiven <mdruiven@users.noreply.github.com>
2016-12-01 21:16:43 -0500

    Update MarkIIIledTest.ino

--- MarkIIIledTest/MarkIIIledTest.ino
+++ MarkIIIledTest/MarkIIIledTest.ino
@@ -3,12 +3,12 @@
  */
 
 void setup() {                
-
-  pinMode(11, OUTPUT);
+  // These are dignostic LEDs which can be used to give a visual signal of an event.
+  pinMode(11, OUTPUT); // The LEDs are connected to pins 11, 12 and 13.
   pinMode(12, OUTPUT);
   pinMode(13, OUTPUT);     
 }
-
+// Flash the LEDs in sequence
 void loop() {
   for (int x = 11; x<14;x++){
   digitalWrite(x, HIGH);  
