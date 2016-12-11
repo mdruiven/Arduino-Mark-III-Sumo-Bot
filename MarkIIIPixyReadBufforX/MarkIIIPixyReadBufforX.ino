f304d9dd64298ef674557332af27be6f2b217e95
M Druiven <mdruiven@users.noreply.github.com>
2016-12-10 21:37:45 -0500

    Add files via upload
    Simply adds to serial print to show x position of blocks.

---
+++ MarkIIIPixyReadBufforX/MarkIIIPixyReadBufforX.ino
@@ -0,0 +1,57 @@
+//
+// Based on the hello_world program
+//
+// http://cmucam.org/projects/cmucam5/wiki/Hooking_up_Pixy_to_a_Microcontroller_(like_an_Arduino)
+//
+// It prints the detected blocks once per second 
+// and prints the x position of the detected blocks
+//
+   
+#include <SPI.h>  
+#include <Pixy.h>
+
+// This is the main Pixy object 
+Pixy pixy;
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.print("Starting...\n");
+
+  pixy.init();
+}
+
+void loop()
+{ 
+  static int i = 0;
+  int j,xPos;
+  uint16_t blocks;
+  char buf[32]; 
+  
+  // grab blocks!
+  blocks = pixy.getBlocks();
+  
+  // If there are detect blocks, print them!
+  if (blocks)
+  {
+    i++;
+    
+    // do this (print) every 50 frames because printing every
+    // frame would bog down the Arduino
+    if (i%50==0)
+    {
+      sprintf(buf, "Detected %d:\n", blocks);
+      Serial.print(buf);
+      for (j=0; j<blocks; j++)
+      {
+        sprintf(buf, "  block %d: ", j);
+        Serial.print(buf); 
+        pixy.blocks[j].print();
+        xPos = pixy.blocks[j].x;
+        Serial.print("X position = ");
+        Serial.println(xPos);
+      }
+    }
+  }  
+}
+
